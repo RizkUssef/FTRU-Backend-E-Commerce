2f5788ca4564fb0241a71d0b610e814ff4ea49ac
Rizk Ussef <rizkusgt67@gmail.com>
2025-02-27 16:55:27 +0200

    modify the order history page

--- resources/views/pages/Profile/order_history.blade.php
+++ resources/views/pages/Profile/order_history.blade.php
@@ -8,7 +8,7 @@
 
         <section class="next_container">
             <h1 class="info_title">ORDER HISTORY</h1>
-            <div class="order_history_list">
+            {{-- <div class="order_history_list">
                     <div class="image_cont">
                         <img src="../../img/Man/Pants/Odion-pants_Pants-1_694_12.png" alt="">
                     </div>
@@ -56,6 +56,14 @@
                     <h1>YOOKO SHORTS</h1>
                     <p class="disc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt vitae cum in nobis dolore eos facere explicabo sequi! Obcaecati ex veritatis quasi cumque soluta, vitae cum rem quidem quia saepe.</p>
                     <p class="pro_price">300 $</p>
+                </div>
+            </div> --}}
+            <div class="order_history_list_not">
+                <div class="img">
+                    <img src="{{asset("img/Stars/exclamation-mark.png")}}" alt="not">
+                </div>
+                <div class="not_found">
+                    <h1 class="empty">You Don't have an items in your Cart</h1>
                 </div>
             </div>
         </section>
